173e702bae40f74dc8fbc029180b325d1dc93580
swzoey <tmddnnim2da@gmail.com>
2018-05-12 16:20:27 +0900

    exe.sh 파일 수정

--- src/mfcc/exe.sh
+++ src/mfcc/exe.sh
@@ -4,7 +4,7 @@
 
 echo "save sound to wav file"
 
-python soundToWav.wav
+python soundToWav.py
 
 echo "실시간 소리를 wav파일로 저장을 완료하였습니다."
 
@@ -13,4 +13,5 @@ python ceps.py $path
 echo " mfcc 알고리즘을 이용하여 wav파일을 ceps파일로 변환하였습니다." 
 
 python ceps_based_classifier.py
+
 
