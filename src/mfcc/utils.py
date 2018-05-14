0cc2c2aded859624ecc519de005c20d1a5f64851
swzoey <tmddnnim2da@gmail.com>
2018-05-14 20:05:18 +0900

    한글 인식 오류 문제 해결

--- src/mfcc/utils.py
+++ src/mfcc/utils.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 # This code is supporting material for the book
 # Building Machine Learning Systems with Python
 # by Willi Richert and Luis Pedro Coelho
@@ -22,7 +24,7 @@ for d in [DATA_DIR, CHART_DIR]:
         os.mkdir(d)
 
 # Put your directory to the different music genres here
-GENRE_DIR = "~\바탕화면\2018-cap1-7\src\sounds"
+GENRE_DIR = "/home/viewtiful/바탕화면/2018-cap1-7/src"
 GENRE_LIST = ["dog", "gun","dryer","car_horn","break","scream"]
 
 
