989220559a4689f4353514177401eedeed16925b
lynn <kylynn315@naver.com>
2018-05-16 21:09:41 +0900

    main 파일 수정 - scikitlearn 셔플 함수 안되는 문제 해결 및 test case 없이 train data로만 진행되는 코드 작성

--- src/mfcc/main.py
+++ src/mfcc/main.py
@@ -26,81 +26,21 @@ genre_list = GENRE_LIST
 genre_dir = GENRE_DIR
 
 
-def train_model(clf_factory, X, Y, name, plot=False):
-    labels = np.unique(Y)
-
-    cv = ShuffleSplit(
-        n=len(X), n_iter=1, test_size=0.3, indices=True, random_state=0)
-
+def train_model(clf_factory, X, Y):
     train_errors = []
-    test_errors = []
-
-    scores = []
-    pr_scores = defaultdict(list)
-    precisions, recalls, thresholds = defaultdict(
-        list), defaultdict(list), defaultdict(list)
-
-    roc_scores = defaultdict(list)
-    tprs = defaultdict(list)
-    fprs = defaultdict(list)
 
     clfs = []  # just to later get the median
 
-    cms = []
-    for train, test in cv:
-        X_train, y_train = X[train], Y[train]
-        X_test, y_test = X[test], Y[test]
-        clf = clf_factory()
+    clf = clf_factory()
 
-        clf.fit(X_train, y_train)
-        clfs.append(clf)
+    clf.fit(X, Y)
+    clfs.append(clf)
 
-        train_score = clf.score(X_train, y_train)
-        test_score = clf.score(X_test, y_test)
-        scores.append(test_score)
+    train_score = clf.score(X, Y)
 
-        train_errors.append(1 - train_score)
-        test_errors.append(1 - test_score)
+    train_errors.append(1 - train_score)
 
-        y_pred = clf.predict(X_test)
-
-        cm = confusion_matrix(y_test, y_pred)
-
-        cms.append(cm)
-
-        for label in labels:
-            y_label_test = np.asarray(y_test == label, dtype=int)
-            proba = clf.predict_proba(X_test)
-            proba_label = proba[:, label]
-
-            precision, recall, pr_thresholds = precision_recall_curve(
-                y_label_test, proba_label)
-            pr_scores[label].append(auc(recall, precision))
-            precisions[label].append(precision)
-            recalls[label].append(recall)
-            thresholds[label].append(pr_thresholds)
-
-            fpr, tpr, roc_thresholds = roc_curve(y_label_test, proba_label)
-            roc_scores[label].append(auc(fpr, tpr))
-            tprs[label].append(tpr)
-            fprs[label].append(fpr)
-
-    if plot:
-        for label in labels:
-            print("Plotting %s" % genre_list[label])
-            scores_to_sort = roc_scores[label]
-            median = np.argsort(scores_to_sort)[len(scores_to_sort) / 2]
-
-            desc = "%s %s" % (name, genre_list[label])
-      #      plot_roc(roc_scores[label][median], desc, tprs[label][median],
-       #              fprs[label][median], label='%s vs rest' % genre_list[label])
-
-    all_pr_scores = np.asarray(pr_scores.values()).flatten()
-    summary = (np.mean(scores), np.std(scores),
-               np.mean(all_pr_scores), np.std(all_pr_scores))
-    print("%.3f\t%.3f\t%.3f\t%.3f\t" % summary)
-
-    return np.mean(train_errors), np.mean(test_errors), np.asarray(cms), clf
+    return np.mean(train_errors),  clf
 
 
 def create_model():
@@ -127,10 +67,6 @@ def create_ceps(fn):
 
     ceps, mspec, spec = mfcc(X)
 
-    #base_fn, ext = os.path.splitext(fn)
-    #data_fn = base_fn + ".ceps"
-    #np.save(data_fn, ceps)
-    #print("Written %s"%data_fn)
     return ceps
 
 
@@ -139,17 +75,27 @@ if __name__ == "__main__":
     wavfile_name = "file"
     X, y = read_ceps(genre_list)
 
-    train_avg, test_avg, cms ,clfss= train_model(
-        create_model, X, y, "Log Reg CEPS", plot=True)
-
-    cm_avg = np.mean(cms, axis=0)
-    cm_norm = cm_avg / np.sum(cm_avg, axis=0)
-
-    plot_confusion_matrix(cm_norm, genre_list, "ceps",
-                          "Confusion matrix of a CEPS based classifier")
+    train_avg,clfss =train_model(
+            create_model, X, y)
 
     DIR = "C:\Users\lynn\PycharmProjects\\2018-cap1-7\src\mfcc"
-    while 1 :
+
+    sub_dir = raw_input("sub_dir : ")
+    fn ="*.ceps.npy"
+
+    af = read_files(fn,sub_dir)
+
+    arr_c = clfss.predict(af)
+
+    print("-----------------------")
+    print("-----------------------")
+    print(arr_c)
+    for i in arr_c :
+        print (genre_list[i])
+    print("-----------------------")
+    print("-----------------------")
+
+"""while 1 :
         make_wav("file", wavfile_num)
         os.chdir(DIR)
         glob_wav = os.path.join(sys.argv[1], wavfile_name+str(wavfile_num)+".wav")
@@ -162,4 +108,4 @@ if __name__ == "__main__":
         print("-----------------------")
         print("-----------------------")
 
-        wavfile_num += 1
+        wavfile_num += 1"""
