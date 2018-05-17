93b36dd90f5b514e78002e69d292e08bc4888ce9
swzoey <tmddnnim2da@gmail.com>
2018-05-17 19:25:12 +0900

    exe.sh 내용 수정

--- src/mfcc/exe.sh
+++ src/mfcc/exe.sh
@@ -2,16 +2,7 @@
 
 #exe.sh
 
-echo "save sound to wav file"
+echo "스마트 안경 착용을 시작합니다"
 
-python soundToWav.py
-
-echo "실시간 소리를 wav파일로 저장을 완료하였습니다."
-
-python ceps.py $path
-
-echo " mfcc 알고리즘을 이용하여 wav파일을 ceps파일로 변환하였습니다." 
-
-python ceps_based_classifier.py
-
+python main.py
 
