747ba87c6b8ee020760ef1a723c1e935b7a57874
swzoey <tmddnnim2da@gmail.com>
2018-05-17 20:27:18 +0900

    added os.system

--- src/display/displaytext.py
+++ src/display/displaytext.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
+#system control
+import sys
+import os
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(15, GPIO.IN)
 count = 0
@@ -8,6 +12,7 @@ while True :
     value = GPIO.input(15)
     print ("value is ", value)
     if value == False :
+        os.system('python transcribe_streaming_mic_gaeul.py')
         count = count + 1
         print count
     time.sleep(0.1)
