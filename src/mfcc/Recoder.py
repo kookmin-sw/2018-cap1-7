6bed32238ffdab34c82d08716de6d876fc979c7a
lynn <kylynn315@naver.com>
2018-05-19 20:05:10 +0900

    녹음파일 생성

---
+++ src/mfcc/Recoder.py
@@ -0,0 +1,16 @@
+
+from save import make_wav
+from ceps import create_ceps
+
+if __name__ == "__main__":
+
+    DIR = "/home/viewtiful/2018-cap1-7/src/mfcc/"
+    #    DIR = "C:\Users\lynn\PycharmProjects\\2018-cap1-7\src\mfcc"
+
+    f = "dog"
+    while 1:
+        num = int(input())
+        make_wav(f, num)
+        fn = DIR + f + str(num) + ".wav"
+        X = []
+        create_ceps(fn)
