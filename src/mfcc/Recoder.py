3fbb8706805b5837797edb95533a4d670aa6051d
KimYealynn <kylynn315@naver.com>
2018-05-19 22:06:03 +0900

    REC, 파일추가
    Signed-off-by: KimYealynn <kylynn315@naver.com>

---
+++ src/mfcc/Recoder.py
@@ -0,0 +1,17 @@
+import sys
+from save import make_wav
+from ceps import create_ceps
+
+if __name__ == "__main__":
+
+    DIR = "/home/viewtiful/2018-cap1-7/src/mfcc/"
+    #    DIR = "C:\Users\lynn\PycharmProjects\\2018-cap1-7\src\mfcc"
+
+    f = sys.argv[1]
+    while 1:
+        num = int(input())
+        sec = int(input())
+        make_wav(f, num, sec)
+        fn = DIR + f + str(num) + ".wav"
+        X = []
+        create_ceps(fn)
