49d79b779add6469e507deb5fb77057cf3bc86eb
GaeulG <gaeulgo2@gmail.com>
2018-05-19 16:21:52 +0900

    added time for 2 position

--- src/display/real_time_audio_processing.py
+++ src/display/real_time_audio_processing.py
@@ -13,7 +13,7 @@ p=pyaudio.PyAudio() # start the PyAudio class
 # convert analog signal to digital signal(Int16) (uses default input device)  
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) 
-
+time.sleep(1)
 while True: # go for infinite loop
     # convet BufferdData(int) to String for knowing audio level value 
     # steam.read(Slieced Data)
@@ -21,12 +21,13 @@ while True: # go for infinite loop
     # Audio level meter -> peak average method
     peak=np.average(np.abs(data))*2
 
-
+    print peak
     # visualize values by bars
-    bars="l"*int(50*peak/2**16)
-    print("%04d %s"%(peak,bars)) # output 
-    os.system("sudo ./show %s" %bars)
-    time.sleep(0.5)
+    if peak > 2000 : 
+        bars="l"*int(50*peak/2**16)
+        print("%04d %s"%(peak,bars)) # output 
+        os.system("sudo ./show %s" %bars)
+        time.sleep(0.12)
         
 
 # stop stream
